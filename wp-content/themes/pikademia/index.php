5d3e831f8fbf05a3cebfd2ecf21129d72f81b839
pikademia <pikademia@gmail.com>
2022-05-09 10:15:46 +0200

    custom WP_Query do wyswietlania 3 ostatnich artykulow

--- wp-content/themes/pikademia/index.php
+++ wp-content/themes/pikademia/index.php
@@ -1,5 +1,37 @@
 <?php
 get_header();
 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+
+		the_title();
+        echo '</br>';
+        the_content();
+        echo '</br>';
+	} 
+} 
+
+
+$args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'posts_per_page'=>3,
+    'order'=>'DESC'
+);
+
+$the_query = new WP_Query($args);
+if($the_query -> have_posts()){
+    while($the_query -> have_posts()){
+        $the_query -> the_post();
+        the_title();
+        echo '</br>';
+        the_content();
+        echo '</br>';
+    }
+}
+
+wp_reset_postdata();
+
 
 get_footer();
